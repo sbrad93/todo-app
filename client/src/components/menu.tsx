f1d193b567ae59998e9766b0b735c5d4c868d672
sbrad93 <shannon.brady2@marist.edu>
2022-11-21 13:43:40 -0500

    created edit-todo view

--- client/src/components/menu.tsx
+++ client/src/components/menu.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { Button, StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
 
 interface IMenu {
     onAddTodo: () => void
@@ -8,21 +9,20 @@ interface IMenu {
 export default function Menu (props: IMenu) {
   return (
     <View style={styles.container}>
-      <Button 
-        title="+ Add Task" 
-        onPress={() => console.log("add todo")}/>
+        <Button  
+            mode="contained" 
+            color="#1E1A3C"
+            onPress={() => props.onAddTodo()}>
+            + Add Task
+        </Button>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
     container: {
-      width: '100%', height: 'auto',
-      color: 'black',
-      alignItems: 'center',
+      width: '100%',
       justifyContent: 'center',
-      display: 'flex',
-      flexDirection: 'row',
-      padding: 20,
+      padding: 40,
     },
   });
---
+++ client/src/screens/edit-todo.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react"
+import { Modal, StyleSheet, View, Text, TouchableOpacity, KeyboardAvoidingView, ScrollView } from 'react-native';
+import ITodo from "../models/todo";
+import { TextInput, Button } from 'react-native-paper';
+
+interface IEditTodoProps {
+  isVisible: boolean
+  onClose: () => void
+  onSave: (data: ITodo) => void
+  data?: ITodo
+}
+
+export default function EditTodoView (props: IEditTodoProps)  {
+  const header = props.data ? 'Edit Todo' : 'Add Todo'
+  const [title, setTitle] = useState(props.data?.title || '')
+
+
+  const onSave = () => {
+    console.log(title);
+    if (title.trim().length === 0) {
+      props.onClose();
+      setTitle('');
+      return;
+    }
+    
+    if (props.data) {
+      const newData = {
+        ...props.data,
+        title
+      }
+      props.onSave(newData);
+    } else {
+      const newData = {
+        id: Math.floor(Math.random() * 10000000)+"",
+        title,
+        isCompleted: false,
+      }
+      props.onSave(newData);
+    }
+  }
+
+  return (
+    <Modal visible={props.isVisible} 
+            style={styles.modal} 
+            animationType="slide">
+      <ScrollView>
+        <KeyboardAvoidingView 
+            style={styles.container}
+            behavior={"padding"}>
+            <Text style={styles.title}>{header}</Text>
+            <View>
+                <TextInput
+                    label={"Title"}
+                        style={styles.input}
+                        onChangeText={setTitle}
+                        value={props.data?.title}
+                        theme={{ colors: { placeholder: 'gray', 
+                                                text: 'black', 
+                                                primary: '#1E1A3C'}}}
+                />
+            </View>
+            <Button  
+                style={styles.btn}
+                mode="contained" 
+                color="#1E1A3C"
+                onPress={onSave}>
+                Save
+            </Button>
+            <Button  
+                style={[styles.btn, { marginTop: 10 }]}
+                mode="outlined" 
+                color="#1E1A3C"
+                onPress={() => props.onClose()}>
+                Cancel
+            </Button>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </Modal>
+  );
+}
+
+
+const styles = StyleSheet.create({
+    modal: {
+        backgroundColor: 'red'
+    },
+    container: {
+      flex: 1,
+      width: '100%', 
+      height: '100%',
+      backgroundColor: '#fff',
+      flexDirection: 'column',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        padding: 20,
+        paddingBottom: 0,
+        marginTop: 50,
+        marginBottom: 10,
+        color: '#1E1A3C'
+    },
+    btn: {
+      margin: 50,
+      marginBottom: 0
+    },
+    input: {
+        marginTop: 20,
+        marginBottom: 0,
+        margin: 15,
+        backgroundColor: '#fff',
+    },
+  });
